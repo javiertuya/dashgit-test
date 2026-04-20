bfa742c4ad395f97a1c756cbdd1f597d7d6588d2
runner <runner@runnervmeorf1.gsy2v5g5vcjefpjhwj5tqngg5b.ex.internal.cloudapp.net>
2026-04-20 20:17:26 +0000

    Reset default test branch content for null

--- main-script.sh
+++ main-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "this is <d0>0.0.1</d0>"
+echo "this is <d0>0.0.0</d0>"
 echo "this is <d1>1.1.1</d1>"
-echo "this is <d2>2.2.3</d2>"
+echo "this is <d2>2.2.2</d2>"
 echo "ending"
 sleep 5
